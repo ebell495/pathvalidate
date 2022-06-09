4eef44d97c47444bfb702a4517c8f85e5d5fa9b5
ebell495 <ericbell495@gmail.com>
2022-06-09 00:33:21 -0700

    Update Fuzz Harness

--- fuzz/fuzz.py
+++ fuzz/fuzz.py
@@ -2,19 +2,33 @@
 import atheris
 import sys
 from pathvalidate import sanitize_filename, sanitize_filepath, validate_filename, is_valid_filename
+from pathvalidate.error import ValidationError
 
 @atheris.instrument_func
 def TestOneInput(data):
-    barray = bytearray(data)
-    if len(barray) > 0:
-        opt = barray[0]
-        del barray[0]
-        if opt % 2 == 0:
-            sanitize_filename(str(barray))
-        elif opt % 2 == 1:
-            is_valid_filename(str(barray))
-    else:
-        sanitize_filename(str(barray))
+    fdp = atheris.FuzzedDataProvider(data)
+    
+    if len(data) < 1:
+        return
+
+    option = fdp.ConsumeBytes(1)[0]
+    in_string = fdp.ConsumeUnicodeNoSurrogates(len(data))
+
+    if option % 4 == 0:
+        sanitize_filename(in_string)
+    elif option % 4 == 1:
+        is_valid_filename(in_string)
+    elif option % 4 == 2:
+        try:
+            sanitize_filepath(in_string)
+        except ValidationError:
+            pass
+    elif option % 4 == 3:
+        try:
+            validate_filename(in_string)
+        except ValidationError:
+            pass
+
 
 atheris.instrument_all()
 atheris.Setup(sys.argv, TestOneInput)
